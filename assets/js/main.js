e197b89811850f7d253635618d1851ce71534dbc
Andrew Hercules <andrewhercules@gmail.com>
2022-09-10 17:02:43 +0100

    Removed JS code that is not used

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  // hide navbar brand element text - Andrew Hercules - on index page
-  let currentUrl = location.pathname;
-  if (currentUrl == "/") {
-    $(".navbar-brand").hide();
-  }
-
-});
